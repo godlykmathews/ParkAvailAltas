b26f6b404a4619267e8a8fa8e2d080bcd32ead05
Godly K Mathews <godlykmathews@outlook.com>
2024-10-20 14:34:51 +0530

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017");
+    await mongoose.connect("mongodb+srv://workgkm:thisisgkm@clusterparkingapp.htl7b.mongodb.net/?retryWrites=true&w=majority&appName=ClusterParkingApp");
     console.log('MongoDB connected');
   } catch (error) {
     console.error('Database connection failed:', error);
